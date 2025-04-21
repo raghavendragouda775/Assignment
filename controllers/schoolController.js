a8788a8a527fb49e7d6f688f67a1fd4daad38852
Raghavendragouda <raghudbose@gmail.com>
2025-04-22 01:12:19 +0530

    Update schoolController.js

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,11 +1,7 @@
-// controllers/schoolController.js
-
 import db from "../config/db.js";
 import calculateDistance from "../utils/distance.js";
-
 export const addSchool = async (req, res) => {
   const { name, address, latitude, longitude } = req.body;
-
   if (!name || !address || !latitude || !longitude) {
     return res.status(400).json({ error: 'All fields are required' });
   }
@@ -13,7 +9,6 @@ export const addSchool = async (req, res) => {
   if (isNaN(latitude) || isNaN(longitude)) {
     return res.status(400).json({ error: 'Latitude and Longitude must be numbers' });
   }
-
   try {
     const [result] = await db.execute(
       'INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)',
@@ -25,7 +20,6 @@ export const addSchool = async (req, res) => {
     res.status(500).json({ error: 'Database error', details: err });
   }
 };
-
 export const listofSchools = async (req, res) => {
   const userLat = parseFloat(req.query.latitude);
   const userLon = parseFloat(req.query.longitude);
