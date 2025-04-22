06d75d45f402a667faca4b48a5c84945ba48c72b
Raghavendragouda <raghudbose@gmail.com>
2025-04-22 23:37:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,11 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
-<<<<<<< HEAD
-=======
+
 
 // Using import.meta.url to resolve paths in ES modules
 import { fileURLToPath } from 'url';
->>>>>>> ec04fce24c63803d58592467b9c2df2d0d2f208d
+
 
 dotenv.config();
 
@@ -21,18 +20,12 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-<<<<<<< HEAD
+
     ca: fs.readFileSync(path.join(__dirname, '../certs/DB-ssl-public-cert.cert')),  
     rejectUnauthorized: false,  
   },
   
 });
 
-=======
-    ca: fs.readFileSync(path.join(__dirname, '../certs/DB-ssl-public-cert.cert')),  // Use the absolute path
-    rejectUnauthorized: false,
-  },
-});
->>>>>>> ec04fce24c63803d58592467b9c2df2d0d2f208d
 
 export default pool.promise();
