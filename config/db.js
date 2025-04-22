ec04fce24c63803d58592467b9c2df2d0d2f208d
Raghavendragouda <raghudbose@gmail.com>
2025-04-22 23:22:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,14 @@ import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
 
+// Using import.meta.url to resolve paths in ES modules
+import { fileURLToPath } from 'url';
+
 dotenv.config();
+
+// Get the current directory path from import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -11,11 +18,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    ca: fs.readFileSync(path.join(__dirname, '../certs/DB-ssl-public-cert.cert')),  
-    rejectUnauthorized: false,  
+    ca: fs.readFileSync(path.join(__dirname, '../certs/DB-ssl-public-cert.cert')),  // Use the absolute path
+    rejectUnauthorized: false,
   },
-  
 });
-
 
 export default pool.promise();
